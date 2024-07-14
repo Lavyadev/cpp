aebeacdeccf0b1ee27c6914a49f58ff19e53f74d
Lavyadev <lavyadevs@gmail.com>
2024-07-14 08:39:19 +0530

    basics on 13.07.24

---
+++ basics/shape.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout <<"   / |"<< endl;
+    cout <<"  /  |"<< endl;
+    cout <<" /   |"<< endl;
+    cout <<"/____|"<< endl;
+     
+    cout <<"   / |" ;
+    cout <<"  /  |";
+    cout <<" /   |";
+    cout <<"/____|";
+
+}
