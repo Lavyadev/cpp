20e03bb749286f128b3af76f018473dc60596e24
Lavyadev <lavyadevs@gmail.com>
2024-07-14 16:49:59 +0530

    strings done

--- basics/strings.cpp
+++ basics/strings.cpp
@@ -3,8 +3,19 @@ using namespace std;
 int main()
 {
     string name = "Lavya Parashar";
-    cout << name.length();
+    /*cout << name.length();
     cout << "\n" << name[4];
     name[0] = 'V';
-    cout << "\n" << name;
+    cout << "\n" << name;*/
+
+    /*cout << name.find("P", 0) << endl;
+    cout << name.find("Parashar", 6);*/
+
+    cout << name.substr(8,3);
+
+    string name1 = name.substr(8,3);
+    cout << "\n" << name1;
+    
+
+    return 0;
 }
