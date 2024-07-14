9acc93e1ed9e734d0c2002fd767db92746c117aa
Lavyadev <lavyadevs@gmail.com>
2024-07-14 09:16:28 +0530

    work of 14.07.24

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 - Made README file
 - Made hello_world.cpp -- Learned basic cpp program of hello world.
 - Made shape.cpp -- Learned basic use of cout and endl.
+
+## 14 july 2024
+
+- Made variables.cpp -- Learned declaration of variables and their uses.
+- Made data_types.cpp -- Learned different types of data types in c++.
---
+++ basics/variables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name = "John" ;
+    int age;
+    age = 35;
+
+    cout << "My name is " << name << endl;
+    cout << "I am " << age <<" years old" << endl;
+
+    name = "tom";
+    cout << name << " is a very good man and " << name <<" should be rewarded." << endl;
+    return 0;
+}
