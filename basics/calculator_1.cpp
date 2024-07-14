e555cfb00f303b4b695bde4423520cc839018ea1
Lavyadev <lavyadevs@gmail.com>
2024-07-14 23:37:08 +0530

    made a basic calc

---
+++ basics/calculator_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num1, num2;
+    cout << "Enter the first number : " ;
+    cin >> num1;
+    cout << "\n Enter the second number : ";
+    cin >> num2;
+
+    cout << "Addition : " << num1 + num2 << endl;
+    cout << "Subtraction : " << num1 - num2 << endl;
+    cout << "Multiplication : " << num1 * num2 << endl;
+    cout << "Division : " << num1 / num2 << endl;
+    cout << "Remainder  : " << int(num1) % int(num2) << endl;
+     
+
+
+
+
+}
